156fbb4c6aaad5530a0de99c31b27fed246a700a
Bryan Chua <me@bryanchua.com>
2020-04-25 13:11:54 +0800

    first push

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const winston = require('winston');
+
+mongoose.connect(process.env.MONGODB_URL);
+mongoose.Promise = global.Promise;
+
+mongoose.connection.on('connected', () => {
+  winston.info('Mongoose connected!');
+});
+
+mongoose.connection.on('disconnected', () => {
+  winston.info('Mongoose disconnected!');
+});
+
+mongoose.connection.on('error', (err) => {
+  winston.error(err.message);
+  process.exit(1);
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'It works!' });
+});
+
+app.listen(8081, () => {
+  console.log('running port 8081');
+});
