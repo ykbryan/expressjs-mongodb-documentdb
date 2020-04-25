976026d4efaeefcc381768f8ed099d3ae8683c46
Bryan Chua <me@bryanchua.com>
2020-04-25 13:32:00 +0800

    Update port

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'It works!' });
 });
 
-app.listen(8081, () => {
-  console.log('running port 8081');
+app.listen(3000, () => {
+  console.log('running port 3000');
 });
